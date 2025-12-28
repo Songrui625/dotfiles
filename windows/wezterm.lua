f84054c0f963921b71c0eb587e2dc6573ad1da80
Songrui625 <songrui625@gmail.com>
2025-12-29 00:10:50 +0800

    add wezterm config on windows
    Signed-off-by: Songrui625 <songrui625@gmail.com>

---
+++ windows/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Google (light) (terminal.sexy)'
+config.font = wezterm.font 'Cascadia Mono'
+config.font_size = 14
+
+config.default_prog = { "C:\\Users\\songrui\\AppData\\Local\\Programs\\nu\\bin\\nu.exe" }
+
+return config
